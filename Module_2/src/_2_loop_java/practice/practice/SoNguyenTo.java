61a2f41c703b94d9bc0267262223cc26b097bcac
trongduy282 <dinhtrantrongduy@gmail.com>
2021-10-30 21:11:02 +0700

    so nguyen to

--- Module_2/src/_2_loop_java/practice/practice/SoNguyenTo.java
+++ Module_2/src/_2_loop_java/practice/practice/SoNguyenTo.java
@@ -1,4 +1,30 @@
 package _2_loop_java.practice.practice;
 
+import java.util.Scanner;
+
 public class SoNguyenTo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap so can kiem tra");
+        int n = scanner.nextInt();
+        if(n<2){
+            System.out.println(n + " khong phai la so nguyen to");
+        } else {
+            int i=2;
+            boolean check = true;
+            while (i <= Math.sqrt(n)){
+                if(n%i==0){
+                    check =false;
+                    break;
+                }
+                i++;
+            }
+            if(check){
+                System.out.println(n + " la so nguyen to");
+            }else {
+                System.out.println(n + " KO la so nguyen to");
+        }
+
+        }
+    }
 }
