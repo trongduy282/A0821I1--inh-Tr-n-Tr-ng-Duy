cb4f2cd08eda6610b2964d1f25a18249c22c9c04
trongduy282 <dinhtrantrongduy@gmail.com>
2022-01-01 11:02:21 +0700

    m

---
+++ Module_3/Bai 1/Bai 1 Co so du lieu quan he.sql
@@ -0,0 +1,18 @@
+create database `student-management`;
+use `student-management`;
+create table student(
+id int,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
+create table class(
+id int,
+`name` varchar(50)
+);
+create table teacher(
+id int,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
