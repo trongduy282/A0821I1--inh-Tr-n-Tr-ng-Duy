41b94884ac176128d14443cef8528581a302683f
trongduy282 <dinhtrantrongduy@gmail.com>
2022-09-04 20:29:00 +0700

    m

--- Module_5/form/app/rating-bar/rating-bar.component.ts
+++ Module_5/form/app/rating-bar/rating-bar.component.ts
@@ -7,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class RatingBarComponent implements OnInit {
   numbers: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  @Input() rate: number;
+  @Input() rating: number;
   constructor() { }
 
   ngOnInit(): void {
